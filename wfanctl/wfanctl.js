cc599934350e526ba62846d659b3649eb82b9ab1
AresDuong <91506129+AresDuong@users.noreply.github.com>
2021-10-20 08:35:06 -0500

    Add files via upload
    1. Go to localhost:3000
    2. See the basic form, set up for the command, "help"
    3. Submit 
    4. See returned help or error
    Tutorial: https://heynode.com/tutorial/process-user-login-form-expressjs/

--- wfanctl/wfanctl.js
+++ wfanctl/wfanctl.js
@@ -1,11 +1,33 @@
+// express is used to access the server
 const express = require('express')
 const app = express()
+// bodyParser is used to access user Input
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false}))
+// go to "localhost:3000"
 const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+})
+// When a user connects to the address that ends in "/" or "" after the port number
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/input.html')
+})
+// Input from "/"
+app.post('/', (req, res) => {
+  let userInput = req.body.userInput
+  switch (userInput) {
+    case 'h':
+    case 'help':
+      res.send('wfanctl version X<br>' +
+               'quit&emsp;Terminate command line mode.')
+    case 'v':
+    case 'd':
+    case 'I':
+    case 'i':
+    case 'f':
+      break
+    default:
+      res.send('Unrecognized command, \"' + userInput +'".')
+  }
 })
